4cd35e691003684ddf691491dffa849ab3c0a143
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-14 14:39:50 -0400

    Completed exercise

--- bank-account.py
+++ bank-account.py
@@ -17,6 +17,32 @@ class BankAccount:
     cls.accounts.append(account)
     return account
 
+  @classmethod
+  def total_funds(cls):
+    total = 0
+    for account in cls.accounts:
+      total += account.balance
+    return total
+
+  @classmethod
+  def interest_time(cls):
+    for account in cls.accounts:
+      account.balance *= (1 + cls.interest_rate)
+
+
 my_account = BankAccount.create()
-your_account = 
+your_account = BankAccount.create()
 print(my_account.balance)
+print(BankAccount.total_funds())
+my_account.deposit(200)
+your_account.deposit(1000)
+print(my_account.balance)
+print(your_account.balance)
+print(BankAccount.total_funds())
+BankAccount.interest_time()
+print(my_account.balance)
+print(your_account.balance)
+print(BankAccount.total_funds())
+my_account.withdraw(50)
+print(my_account.balance)
+print(BankAccount.total_funds())
