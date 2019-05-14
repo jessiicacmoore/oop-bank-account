d6ae38d9706b5b874937c0a76e3d4f40db61ef8d
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-14 18:52:45 -0400

    Outlined the Vampire class

---
+++ vampires/vampire.py
@@ -0,0 +1,23 @@
+class Vampire:
+  def __init__(self):
+    pass
+
+  @classmethod
+  def create(cls):
+    pass
+
+  @classmethod
+  def sunrise(cls):
+    pass
+
+  @classmethod
+  def sunset(cls):
+    pass
+
+  def drink_blood(self):
+    pass
+
+  def go_home(self):
+    pass
+
+  
