5a7aa0bf993053f3520bc9d0e0301cb528b9d02b
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-15 10:51:31 -0400

    Created class method of create

--- vampires/vampire.py
+++ vampires/vampire.py
@@ -1,10 +1,16 @@
 class Vampire:
-  def __init__(self):
-    pass
+
+  coven = []
+
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+    self.in_coffin = True
 
   @classmethod
-  def create(cls):
-    pass
+  def create(cls, name, age):
+    vampire = Vampire (name, age)
+    cls.coven.append(vampire)
 
   @classmethod
   def sunrise(cls):
